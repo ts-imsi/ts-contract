fd0279f18c4b3a3b9fd7447f4c5d3dbc32e6e11d
luoyunsak <sakfi102012@163.com>
2017-09-27 16:52:56 +0800

    #合同增加联系电话

--- src/main/java/com/trasen/tscontract/model/OaContractInfo.java
+++ src/main/java/com/trasen/tscontract/model/OaContractInfo.java
@@ -7,57 +7,59 @@ public class OaContractInfo {
 
     private Integer id;
 
-    private String contractNo;
+    private String contractNo; //合同编号
 
-    private Integer contractType;
+    private Integer contractType;//合同类型
 
-    private String contractName;
+    private String contractName;//合同名称
 
-    private Integer contractPrice;
+    private Integer contractPrice;//合同金额
 
-    private Integer productType;
+    private Integer productType;//产品分类(1=软件，2=硬件，3=系统集成，4=服务，5=其他)
 
-    private Integer buyPrice;
+    private Integer buyPrice;//外购支付
 
-    private Date signDate;
+    private Date signDate;//签约时间
 
-    private String contractOwner;
+    private String contractOwner;//合同所有者
 
-    private String buyer;
+    private String buyer;//甲方
 
-    private String seller;
+    private String seller;//乙方
 
-    private String buyerSigner;
+    private String buyerSigner;//甲方签约人
 
-    private String sellerSigner;
+    private String sellerSigner;//乙方签约人
 
-    private Integer customerId;
+    private Integer customerId;//客户ID
 
-    private Date createTime;
+    private Date createTime;//创建时间
 
-    private Date timeLimit;
+    private Date timeLimit;//完成时限
 
-    private Integer state;
+    private Integer state;//状态(0未执行1执行中2未寄回4已结束)
 
-    private String paymode;
+    private String paymode;//付款方式
 
-    private String remark;
+    private String remark;//备注
 
-    private Integer createType;
+    private Integer createType;//创建类型，0=内控创建，1=销售创建
 
-    private String createUserId;
+    private String createUserId;//创建用户
 
-    private Date distributeDate;
+    private Date distributeDate;//分解时间
 
-    private Date maintainLimit;
+    private Date maintainLimit;//维护期限
 
-    private String customerName;
+    private String customerName;//客户名称
 
-    private String customerNo;
+    private String customerNo;//客户编号
 
-    private String hospitalGrade;
+    private String hospitalGrade;//医院等级
 
-    private String contactAddress;
+    private String contactAddress;//医院地址
+
+    private String contactPhone;//联系电话
 
     public Integer getId() {
         return id;
@@ -275,4 +277,11 @@ public class OaContractInfo {
         this.contactAddress = contactAddress;
     }
 
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
 }
